c02beb7496ad216d6d53030e15416fb9a09d156f
Hyogan <tiaghojeux@gmail.com>
2026-07-10 17:16:50 +0100

    refactor: replace Expo starter template with UniWind setup
    - remove tabs and template components
    - configure UniWind
    - add global styles
    - update assets and app layout
    - clean project structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,21 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { StatusBar } from "react-native";
+import "../global.css"; // <-- file from previous step
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+// import "../global.css";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  //  <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+    <SafeAreaView style={{ flex: 1 }}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+      <StatusBar barStyle="default" />
+    </SafeAreaView>
+
+    // {/* </ThemeProvider> */}
   );
 }
